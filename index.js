a5fcc04eda0842c8a00c872e8d4a53c37f28603b
Jack Vu <jacknvu98@gmail.com>
2024-03-01 19:21:41 -0500

    finished array functions and spread operator

--- index.js
+++ index.js
@@ -36,5 +36,101 @@ array.pop(); // [ 'first index', 'second index', 'third index', 'forth index' ]
 // the second argument which is optional is the number of items you want to remove based starting from the index you indicated first
 // the third and follow arguments are optional and are to add new element(s) to the array from the indicated index of the first argument
 array.splice(1, 1, "Replacing the second index"); // ['first index','Replacing the second index','third index','forth index',]
+array.splice(1, 1, "second index"); // ['first index','second index','third index','forth index',]
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Array Iteration Functions
+
+// Higher order functions are functions that accepts or return other functions
+
+// calculate is an example that accepts a function as an argument and returns the function as well
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const calculate = (num1, num2, operation) => {
+  return operation(num1, num2);
+};
+
+// forEach() is calls a callback function once for each array element you can think of it as the block of code will execute for each element of the array
+// the expected outcome in your console should be for just the function above should be this:
+// first index
+// second index
+// third index
+// forth index
+array.forEach((element) => {
+  console.log(element); // this will log each element of the array
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+// map() creates a new array performing a function on each of the array elements
+// map also calls a callback function for each element of the array
+// this array function does not affect the original array
+// usually we would assign the newly created array made by the map function to make use of that new array
+// numbersDoubled should look like this based on the numbers array : [ 1, 4, 9, 16, 25 ]
+const numbersDoubled = numbers.map((number) => {
+  return number * number;
+});
+
+// filter() also creates a new array, but only elements that pass the requirements of the function
+// the below function should create a new array with only the even numbers of the numbers array this should look like this [ 2, 4 ]
+const evenNumbers = numbers.filter((number) => number % 2 === 0); // if you are creating a call back function that returns a single line you can write it like this
+
+// For these functions you can combine them to apply multiple array functions at a time
+
+// reduce() will go through an array and reduce it to a single value
+// calculating the sum
+// the first argument is a callback function that uses the accumulator and the currentValue each iteration
+// the optional you can set the initial value for the accumulator and the accumulator will start with that value
+// if nothing is places in this optional argument the accumulator will use the first element of the array as the initial value and start the iterations from the second element
+const sumOfNumbers = numbers.reduce((a, c) => a + c); // the result of sumOfNumbers should be 15
+
+// indexOf() searches for an element in the array and returns the index of that element in the array if it is found
+// if there are multiple elements that match the search the first one's index will be returned
+// the arguments taken in is the element to search for and an optional argument to state which index do you want to start the search
+const findNumberFive = numbers.indexOf(5); // this should return the number 4 which is the index of 5 in the numbers array
+
+// includes() this checks to see if the argument exist in the array it will search the array and return a boolean value
+const doesSixExist = numbers.includes(6); // this should return false since 6 is not in the numbers array
+
+const randomWords = [
+  "Hello",
+  "my",
+  "name",
+  "is",
+  "Jack",
+  "and",
+  "I",
+  "love",
+  "coding",
+];
+// sort() the is function mutates the original array you can sort string and number elements
+// when used on an array of strings you can plainly use .sort()
+// the sorting below cause randomWords to turn into this
+// [
+//   'Hello',  'I',
+//   'Jack',   'and',
+//   'coding', 'is',
+//   'love',   'my',
+//   'name'
+// ]
+randomWords.sort();
+
+// but for numbers you require a compare function  this function will return negative, zero, or a positive number and will sort the values accordingly
+// traditionally we use a - b for ascending order and b - a for descending order
+// the below sorting will sort all the numbers appropriately into this
+// [
+//   1, 2,  3,   5, 6,
+//   8, 9, 21, 111
+// ]
+const randomNumbers = [1, 5, 6, 21, 3, 8, 2, 111, 9];
+randomNumbers.sort((a, b) => a - b);
+
+// a common operation we have used with manipulating an array is spread operator
+// an easy way to duplicate the same array twice with the same numbers is with the spread operator
+
+const doubleNumbers = [...numbers, ...numbers]; // [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+
+// spread isn't a array specific function, but it is something we have used and I wanted to note it as memory here
